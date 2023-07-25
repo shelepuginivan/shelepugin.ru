a6352bccd76cb35614f77b3ba1b8f4208d536882
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-25 09:55:48 +0300

    feat: add Sentry integration

--- next.config.js
+++ next.config.js
@@ -4,3 +4,24 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require('@sentry/nextjs')
+
+module.exports = withSentryConfig(
+	module.exports,
+	{
+		silent: true,
+		org: process.env.SENTRY_ORG,
+		project: process.env.SENTRY_PROJECT,
+	},
+	{
+		widenClientFileUpload: true,
+		transpileClientSDK: true,
+		tunnelRoute: '/monitoring',
+		hideSourceMaps: true,
+		disableLogger: true,
+	}
+)
