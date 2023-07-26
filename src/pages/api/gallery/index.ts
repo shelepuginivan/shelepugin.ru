e069d9f84ecd657841f341e36455b43a7c6980c0
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-26 10:00:50 +0300

    feat(api): use method `getCategoriesWithPreviews`

--- src/pages/api/gallery/index.ts
+++ src/pages/api/gallery/index.ts
@@ -2,23 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import { GalleryService } from '@/server/GalleryService'
 import { ServerException } from '@/server/ServerException'
-import { GalleryCategory } from '@/utils/types/GalleryCategory'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	try {
-		const categories = await GalleryService.getCategories()
-		const categoriesWithPreview: GalleryCategory[] = []
-
-		for (let i = 0; i < categories.length; i++) {
-			const name = categories[i]
-
-			const category: GalleryCategory = {
-				previewUrl: await GalleryService.getRandomImageUrlByCategory(name),
-				name
-			}
-
-			categoriesWithPreview.push(category)
-		}
+		const categoriesWithPreview = await GalleryService.getCategoriesWithPreviews()
 
 		res.status(200).json(categoriesWithPreview)
 	} catch (error) {
