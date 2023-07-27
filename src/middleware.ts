53df266f02648a56bccdfb869d9ec09d37335e81
shelepuginivan <shelepuginivanm@gmail.com>
2023-07-27 09:37:45 +0300

    feat: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const middleware = (req: NextRequest) => {
+	const methodNotAllowedResponse = new NextResponse(
+		JSON.stringify({ message: 'Неверный метод запроса' }),
+		{
+			status: 405,
+			headers: { 'Content-Type': 'application/json; charset=utf-8' },
+			nextConfig: {}
+		}
+	)
+
+	if (
+		req.nextUrl.pathname !== '/api/feedback' && req.method !== 'GET' ||
+		req.nextUrl.pathname === '/api/feedback' && req.method !== 'POST'
+	) {
+		return methodNotAllowedResponse
+	}
+}
