cffebee14d20266192e302e755c0e467fe1d1f6b
shelepuginivan <shelepuginivanm@gmail.com>
2023-10-19 22:17:43 +0300

    fix(ui): use lucide icons instead of icon font

--- src/ui/ErrorMessage/ErrorMessage.tsx
+++ src/ui/ErrorMessage/ErrorMessage.tsx
@@ -1,3 +1,4 @@
+import { Home } from 'lucide-react'
 import { Nunito, Roboto_Flex } from 'next/font/google'
 import Link from 'next/link'
 import { FC } from 'react'
@@ -23,7 +24,9 @@ const ErrorMessage: FC<PropsType> = ({ message, header }) =>
 	<div className={styles.errorMessage}>
 		<h1 className={nunito.className}>{header ?? 'Произошла ошибка!'}</h1>
 		<p className={robotoFlex.className}>{message}</p>
-		<Link href='/'><i className='icon-home'></i></Link>
+		<Link href='/' aria-label='Перейти на главную страницу'>
+			<Home size={32} aria-hidden={true} focusable={false}/>
+		</Link>
 	</div>
 
 export default ErrorMessage
