e528e71f29d98a2c318c08cc1ee444730d09b9e3
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-09 01:10:41 +0300

    feat(components): add component `ReportBug`

---
+++ src/components/ReportBug/ReportBug.tsx
@@ -0,0 +1,43 @@
+import { Github, Mail } from 'lucide-react'
+import { Nunito, Roboto_Flex } from 'next/font/google'
+import { FC } from 'react'
+
+import styles from './reportBug.module.sass'
+
+const nunito = Nunito({
+	subsets: ['cyrillic'],
+	weight: '300',
+})
+
+const robotoFlex = Roboto_Flex({
+	subsets: ['latin'],
+	weight: '400',
+})
+
+const ReportBug: FC = () => {
+	return (
+		<div className={styles.reportBug}>
+			<h2 className={`${styles.reportBugHeader} ${nunito.className}`}>
+				Нашли баг?
+			</h2>
+			<p className={`${styles.reportBugMessage} ${robotoFlex.className}`}>
+				Если вы столкнулись с проблемой, пожалуйста, опишите её и отправьте
+				любым удобным для вас способом:
+			</p>
+			<ul className={`${styles.linkList} ${robotoFlex.className}`}>
+				<li>
+					<Mail size={20} />
+					<a href='mailto:shelepuginivanm@gmail.com'>На почту</a>
+				</li>
+				<li>
+					<Github size={20} />
+					<a href='https://github.com/shelepuginivan/shelepugin.ru/issues/new'>
+						В GitHub Issues
+					</a>
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default ReportBug
