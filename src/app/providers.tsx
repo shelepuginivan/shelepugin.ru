69645d17b1519c48ea1efd195e960fd14f396f78
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-26 19:48:20 +0300

    style(eslint): sort imports

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { FC, ReactNode, useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { FC, ReactNode, useState } from 'react'
 
 const Providers: FC<{ children: ReactNode }> = ({ children }) => {
 	const [queryClient] = useState(() => new QueryClient())
