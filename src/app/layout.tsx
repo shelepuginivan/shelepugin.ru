0cdc01c01feb2fe1b87d232c19f68ee51d0a8dfc
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-27 00:40:23 +0300

    feat(app, seo): set default app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,24 +3,43 @@ import '@/styles/reset.sass'
 import '@/styles/variables.sass'
 import '@/styles/media.sass'
 
+import { Metadata } from 'next'
+
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
+import { HOST } from '@/utils/constants'
 
 import Providers from './providers'
+
+export const metadata: Metadata = {
+	metadataBase: new URL(HOST),
+	title: {
+		template: '%s | Иван Шелепугин',
+		default: 'Иван Шелепугин',
+	},
+	authors: {
+		name: 'Иван Шелепугин',
+	},
+	creator: 'Иван Шелепугин',
+	icons: {
+		icon: { url: '/favicon.png', type: 'image/png' },
+		shortcut: { url: '/favicon.png', type: 'image/png' },
+	},
+	openGraph: {
+		title: {
+			template: '%s | Иван Шелепугин',
+			default: 'Иван Шелепугин',
+		},
+		locale: 'ru_RU',
+		siteName: 'Иван Шелепугин',
+		images: `${HOST}/favicon.png`,
+	},
+	manifest: '/manifest.json',
+}
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<html lang='ru'>
-			<head>
-				<meta name='charset' content='UTF-8' />
-				<meta name='author' content='Иван Шелепугин' />
-				<meta name='copyright' content='Иван Шелепугин' />
-				<meta name='og:locale' content='ru_RU' />
-				<link rel='manifest' href='/manifest.json' />
-				<link rel='icon' href='/favicon.png' />
-				<link rel='shortcut icon' href='/favicon.png' />
-				<link rel='favicon' href='/favicon.png' />
-			</head>
 			<body>
 				<Header />
 				<Providers>{children}</Providers>
