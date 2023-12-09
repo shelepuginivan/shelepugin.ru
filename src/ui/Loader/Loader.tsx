9af8b69b2703d2e8c4420b5bf40db12917cdae26
shelepuginivan <shelepuginivanm@gmail.com>
2023-12-09 19:20:19 +0300

    feat(ui): set loader attribute `role='status'`

--- src/ui/Loader/Loader.tsx
+++ src/ui/Loader/Loader.tsx
@@ -2,6 +2,6 @@ import { FC } from 'react'
 
 import styles from './loader.module.sass'
 
-const Loader: FC = () => <span className={styles.loader}></span>
+const Loader: FC = () => <span className={styles.loader} role='status'></span>
 
 export default Loader
