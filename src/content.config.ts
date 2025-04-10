8f9f6bdaefc2d29f547de36303346c845079e4dc
shelepuginivan <shelepuginivanm@gmail.com>
2025-04-10 08:28:50 +0300

    content: add recent blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,12 @@
 import { glob } from 'astro/loaders'
 import { defineCollection } from 'astro:content'
 
+const blog = defineCollection({
+    loader: glob({ pattern: '**/*.mdx', base: './src/data/blog' }),
+})
+
 const projects = defineCollection({
     loader: glob({ pattern: '**/*.mdx', base: './src/data/projects' }),
 })
 
-export const collections = { projects }
+export const collections = { blog, projects }
